8d2d0a9db7ca9a8ea0a4efaa813ee881608b898f
haris <bin.talat900@gmail.com>
2023-02-28 10:34:23 +0500

    Changes in bottom nav

--- src/components/Menu/tabMenu.tsx
+++ src/components/Menu/tabMenu.tsx
@@ -9,40 +9,51 @@ const TabMenu: React.FC<IMenuProps> = ({ active }) => {
     <nav className="fixed px-6 bottom-0 w-full flex items-center justify-between border-2 bg-white text-black">
       <ul className="flex-0 mr-5">
         <li className="py-2">
-          <Link href="/" className="flex justify-center">
-            <FaHome color={active === "/" ? "black" : "gray"} />
+          <Link href="/">
+            <div className="flex justify-center">
+              <FaHome color={active === "/" ? "black" : "gray"} size={20} />
+            </div>
+            <p className="text-sm">Home</p>
           </Link>
-          <p className="text-sm">Home</p>
         </li>
       </ul>
       <ul className="flex-0 mr-5">
         <li className="py-2">
-          <Link href="/affirmations/likes" className="flex justify-center">
-            <FaHeart
-              color={active === "/affirmations/likes" ? "black" : "gray"}
-            />
+          <Link href="/affirmations/likes">
+            <div className="flex justify-center">
+              <FaHeart
+                color={active === "/affirmations/likes" ? "black" : "gray"}
+                size={20}
+              />
+            </div>
+            <p className="text-sm">Likes</p>
           </Link>
-          <p className="text-sm">Likes</p>
         </li>
       </ul>
       <ul className="flex-0 mr-5">
         <li className="py-2">
-          <Link href="/affirmations/random" className="flex justify-center">
-            <FaRandom
-              color={active === "/affirmations/random" ? "black" : "gray"}
-            />
+          <Link href="/affirmations/random">
+            <div className="flex justify-center">
+              <FaRandom
+                color={active === "/affirmations/random" ? "black" : "gray"}
+                size={20}
+              />
+            </div>
+            <p className="text-sm">Random</p>
           </Link>
-          <p className="text-sm">Random</p>
         </li>
       </ul>
       <ul className="flex-0 mr-5">
         <li className="py-2">
-          <Link href="/affirmations/search" className="flex justify-center">
-            <FaSearch
-              color={active === "/affirmations/search" ? "black" : "gray"}
-            />
+          <Link href="/affirmations/search">
+            <div className="flex justify-center">
+              <FaSearch
+                color={active === "/affirmations/search" ? "black" : "gray"}
+                size={20}
+              />
+            </div>
+            <p className="text-sm">Search</p>
           </Link>
-          <p className="text-sm">Search</p>
         </li>
       </ul>
     </nav>
